9a66eb7292acc6b639d6776bae4d1945b90d3aec
Rudraksh Raina <1819rudraksh@gmail.com>
2026-08-09 00:12:12 +0530

    user controller and router setup and express connection error solved

--- src/app.js
+++ src/app.js
@@ -8,16 +8,19 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
 app.use(express.json({
     limit:"16kb"
 }))
-
 app.use(express.urlencoded({
     limit: "16kb"
 }))
 app.use(express.static("public"))
+app.use(cookieParser())
 
-app.use(express.cookieParser())
+// IMPORTING ROUTERS
+import userRouter from "./routes/user.routes.js";
+
+// routers declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
